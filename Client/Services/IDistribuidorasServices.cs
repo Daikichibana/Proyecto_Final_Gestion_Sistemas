8a27c3ae4a4f700af0c05ace39d4f262df8c378c
Aldo Larrain <aldo.larrain@hotmail.com>
2022-06-11 12:20:10 -0400

    AdministrarRubro Front

--- Client/Services/IDistribuidorasServices.cs
+++ Client/Services/IDistribuidorasServices.cs
@@ -1,6 +1,18 @@
-﻿namespace Proyecto_Final_Gestion_Sistemas.Client.Services
+﻿using Compartido;
+using Compartido.Dto.Distribuidora.General;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final_Gestion_Sistemas.Client.Services
 {
     public interface IDistribuidorasServices
     {
+
+        #region Rubro
+        Task<ServiceResponse<List<RubroDTO>>> ObtenerTodoRubro();
+        Task<ServiceResponse<RubroDTO>> CrearRubro(RubroDTO Rubro);
+        Task<ServiceResponse<RubroDTO>> ActualizarRubro(RubroDTO Rubro);
+        Task<ServiceResponse<RubroDTO>> EliminarRubro(RubroDTO Rubro);
+        #endregion
     }
 }
