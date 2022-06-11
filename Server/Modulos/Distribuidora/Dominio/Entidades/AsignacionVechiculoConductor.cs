6d88c1e41ea0836a06005e9a4c45fb8345fbac2f
chiba <chiba@LEGION>
2022-06-11 10:24:27 -0400

    Cambios

---
+++ Server/Modulos/Distribuidora/Dominio/Entidades/AsignacionVechiculoConductor.cs
@@ -0,0 +1,7 @@
+﻿namespace Proyecto_Final_Gestion_Sistemas.Server.Modulos.Distribuidora.Dominio.Entidades
+{
+    public class AsignacionVechiculoConductor
+    {
+        
+    }
+}
---
+++ Server/Modulos/Inventario/Dominio/Entidades/Stock.cs
@@ -0,0 +1,7 @@
+﻿namespace Proyecto_Final_Gestion_Sistemas.Server.Modulos.Inventario.Dominio.Entidades
+{
+    public class Stock
+    {
+        
+    }
+}
