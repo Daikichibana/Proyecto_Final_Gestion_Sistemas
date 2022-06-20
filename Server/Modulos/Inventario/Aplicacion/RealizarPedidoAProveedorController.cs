bfa9ac582e9304b7f39f1c8ad2a2a37054db1eb6
Daiki Chibana <a2018112376@estudiantes.upsa.edu.bo>
2022-06-19 22:05:56 -0400

    commit compraproveedor controller

--- Server/Modulos/Inventario/Aplicacion/RealizarPedidoAProveedorController.cs
+++ Server/Modulos/Inventario/Aplicacion/RealizarPedidoAProveedorController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Compartido;
+using Compartido.Dto.Inventario.General;
+using Microsoft.AspNetCore.Mvc;
+using Proyecto_Final_Gestion_Sistemas.Server.Modulos.Inventario.Dominio.Abstracciones;
+using System;
 
 namespace Proyecto_Final_Gestion_Sistemas.Server.Modulos.Inventario.Aplicacion
 {
@@ -6,8 +10,36 @@ namespace Proyecto_Final_Gestion_Sistemas.Server.Modulos.Inventario.Aplicacion
     [ApiController]
     public class RealizarPedidoAProveedorController : ControllerBase
     {
-        public RealizarPedidoAProveedorController()
+        IRealizarPedidoAProveedorService _realizarPedidoAProveedorService;
+        public RealizarPedidoAProveedorController(IRealizarPedidoAProveedorService realizarPedidoAProveedorService)
         {
+            _realizarPedidoAProveedorService = realizarPedidoAProveedorService;
+        }
+
+        [HttpGet]
+         public IActionResult registrarCompraAProveedor(NotaRecepcionDTO notaRecepcionDTO)
+        {
+            var result = new ServiceResponse<NotaRecepcionDTO>();
+
+            try
+            {
+                _realizarPedidoAProveedorService.realizarPedidoProveedor(notaRecepcionDTO);
+
+                result.Data = null;
+                result.Message = "Se ha realizado la operacion correctamente.";
+                result.Success = true;
+
+                return Ok(result);
+
+            }
+            catch (Exception error)
+            {
+                result.Data = null;
+                result.Message = error.Message;
+                result.Success = false;
+
+                return BadRequest(result);
+            }
         }
     }
 }
